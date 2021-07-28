cb8c65462e1a029d267590908d6ef6a07677da53
Marta Pryiom <Vynnytska1989@gmail.com>
2021-07-28 17:20:26 +0300

    Добавила конфіг вепак

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./css/common.scss";
+import name from "./module-1";
+console.log(name);
+
+console.log('ЦЕ index.js')
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+ module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader",],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ],
+  },
+   plugins: [
+       
+       new HtmlWebpackPlugin({ template: './src/index.html' }),
+       new MiniCssExtractPlugin({ filename:'style.css' }),
+
+  ],
+  
+  devServer: {
+      port:5555,
+      open: true,
+      stats:'errors-only',
+  },
+
+}
